b4b5c994028b540b0ea9a3aaded07d70e2b043fc
mariastlouis <msantray@yahoo.com>
2017-10-18 10:47:04 -0600

    additional tests

---
+++ tests/insertion-test.js
@@ -0,0 +1,56 @@
+import { assert } from 'chai';
+import { expect } from 'chai';
+import insertionSort from '../scripts/insertion-sort.js'
+import randomNumberArray from '../scripts/randomNumberArray.js'
+
+describe('TDD with insertionSort', () => {
+  it('should be a function', () => {
+    assert.isFunction(insertionSort);
+  });
+
+
+  it('should sort 2 items ', () => {
+    let array = [ 5, 3 ]
+    assert.equal(array[0] > array[1], true);
+    insertionSort(array);
+    assert.equal(array[0] < array[1], true);
+  });
+
+
+  it('should sort an array with 20 items', () => {
+    let newArray = randomNumberArray(20, 50);
+    let sortedArray = insertionSort(newArray);
+    for (let i = 0; i < sortedArray.length-1; i++) {
+      expect(sortedArray[i] < sortedArray[i+1], true);
+    }
+  });
+
+    it('should sort an array with 1500 items', () => {
+    let newArray = randomNumberArray(1500, 1500);
+    let sortedArray = insertionSort(newArray);
+
+    for (let i = 0; i < sortedArray.length-1; i++) {
+      expect(sortedArray[i] < sortedArray[i+1], true);
+    }
+  });
+
+     it('Should sort an array with 5500 items', () => {
+    let newArray = randomNumberArray(5500, 5500);
+    let sortedArray = insertionSort(newArray);
+    
+    for(let i = 0; i<sortedArray.length; i++){
+    expect(sortedArray[i]<sortedArray[i+1], true);
+    }
+  })
+
+    it('should sort an array with 15000 items', () => {
+    let newArray = randomNumberArray(15000, 15000);
+    let sortedArray = insertionSort(newArray);
+
+    for (let i = 0; i < sortedArray.length-1; i++) {
+      expect(sortedArray[i] < sortedArray[i+1], true);
+    }
+  });
+  
+
+});
---
+++ tests/merge-sort-test.js
@@ -0,0 +1,12 @@
+import { assert } from 'chai';
+import { expect } from 'chai';
+import mergeSort from '../scripts/merge-sort.js'
+import merge from '../scripts/merge-sort.js'
+import randomNumberArray from '../scripts/randomNumberArray.js'
+
+describe('TDD with mergeSort', () => {
+  it('should be a function', () => {
+    assert.isFunction(mergeSort);
+  });
+
+ });
