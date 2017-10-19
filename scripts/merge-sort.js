dfb96d56d0ee2f9807060e1805c593387cd184f1
mariastlouis <msantray@yahoo.com>
2017-10-19 08:52:13 -0600

    fixed math.floor on merge sort

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -2,7 +2,7 @@ const mergeSort = (arr) => {
     if (arr.length <= 1) {
       return arr;
     }
-    let middle = arr.length / 2;
+    let middle = Math.floor(arr.length / 2);
     let left = arr.slice(0, middle);
     let right = arr.slice(middle, arr.length);
     return merge(mergeSort(left), mergeSort(right));
