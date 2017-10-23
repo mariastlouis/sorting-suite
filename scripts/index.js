9b7948de0f490edc4c7c9b7e4272fc24064a5562
mariastlouis <msantray@yahoo.com>
2017-10-23 13:41:28 -0600

    styled changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,6 @@
-
-
 const randomNumberArray = require('./randomNumberArray.js');
 const bubbleSort = require('./bubble-sort.js');
-
 const mergeSort = require('./merge-sort.js');
-
-
 const insertionSort = require('./insertion-sort.js');
 const quickSort = require('./quicksort.js')
 
