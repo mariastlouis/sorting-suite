cb66a4946c78dca419b9e70ecae43f62d97e3a04
mariastlouis <msantray@yahoo.com>
2017-10-23 13:50:31 -0600

    changed file structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const randomNumberArray = require('./scripts/randomNumberArray.js');
+const bubbleSort = require('./scripts/bubble-sort.js');
+const mergeSort = require('./scripts/merge-sort.js');
+const insertionSort = require('./scripts/insertion-sort.js');
+const quickSort = require('./scripts/quicksort.js')
+
+
+module.exports = {
+  randomNumberArray,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  bubbleSort
+}
